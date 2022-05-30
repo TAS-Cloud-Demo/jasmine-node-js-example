6338106c50aeefdef9a7829a086aec22b9589e0e
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-05-30 11:47:54 +0530

    Update calculator.function.spec.js

--- spec/unit/calculator.function.spec.js
+++ spec/unit/calculator.function.spec.js
@@ -30,9 +30,9 @@ describe("Calculator Functions", function() {
       expect(value).toBe(1);
     });
   
-    it.skip("should give the same value when adding zero", function() {
+    it("should give the same value when adding zero", function() {
       let value = calc.add(3, 0);
-      expect(value).toBe(5);
+      expect(value).toBe(3);
     });
   
   });
